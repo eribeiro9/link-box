d2891006376d903e6c0b82cf3afd4c8e4f9a99a0
Eric Ribeiro <eric.james.ribeiro@gmail.com>
2017-03-03 19:37:16 -0500

    Fix

--- automation/src/pages/landing.page.js
+++ automation/src/pages/landing.page.js
@@ -3,7 +3,7 @@ import { Page } from './page';
 export class LandingPage extends Page {
   constructor() {
     super('/', 'at-landing-page');
-    this.loginButton = element(by.className('at-landing-register-btn'));
-    this.registerButton = element(by.className('at-landing-login-btn'));
+    this.loginButton = element(by.className('at-landing-login-btn'));
+    this.registerButton = element(by.className('at-landing-register-btn'));
   }
 }
